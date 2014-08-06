1deafec2b844cb09dcad3189814c38a361d3bb91
Katie <katiehahm@gmail.com>
2014-08-06 13:21:31 +0200

    homepage signup works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    events_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  # def after_sign_up_path_for(resource_or_scope)
+  #    new_user_registration_path
+  #  end
+
 end
