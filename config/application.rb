d7081fe76cc23a8ac0ae06090001e2efe2b38604
rmusa <rafaelpmusa@gmail.com>
2014-08-21 09:34:00 +0200

    activeadmin on heroku

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,14 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module FinalProject
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js'] 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
